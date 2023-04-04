8f088d3988ab5cf2e3f4a3d568b32513bf44d62c
Danushka-Madushan <Danushkamadushan128@gmail.com>
2023-04-04 15:05:04 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const qrcode = require('qrcode-terminal');
+const qrcode = require('qrcode-terminal')
 const axios = require('axios')
-const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
+const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js')
 
 const regex = new RegExp(`https://w{3}\\.henteibruh\\.com/20[0-9]{2}/[0-9]{2}/.+\\.html`, '')
 const urlRegex = new RegExp(`<p>Source.+?href="(https://w{3}.google.com/search\\?.+?)".target="_blank">|<p>Source:(.+?)</p>`, 'g')
@@ -14,18 +14,18 @@ const client = new Client({
 })
 
 client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
-});
+    qrcode.generate(qr, {small: true})
+})
 
 client.on('ready', () => {
-    console.log('Client is ready!');
-});
+    console.log('Client is ready!')
+})
 
-/*
 client.on('message_create', (message) => {
-    console.log(message)
-});
-*/
+    if(message.body === '!ping') {
+		message.reply('pong')
+	}
+})
 
 client.on('message', async (message) => {
     const match = regex.exec(message.body)
@@ -39,16 +39,16 @@ client.on('message', async (message) => {
             await chat.sendMessage(media);
             matches.forEach((match, groupIndex) => {
                 if (gregx.exec(match) !== null) {
-                    client.sendMessage(message.from, ` Souce : ${match}`);
+                    client.sendMessage(message.from, ` Souce : ${match}`)
                 } else {
-                    client.sendMessage(message.from, ` Manual Extract : ${message.body}`);
+                    client.sendMessage(message.from, ` Manual Extract : ${message.body}`)
                 }
-            });
+            })
         } catch(e) {
-            client.sendMessage(message.from, ` Error : ${e.message}`);
+            client.sendMessage(message.from, ` Error : ${e.message}`)
         }
     }
-});
+})
 
 
-client.initialize();
+client.initialize()
