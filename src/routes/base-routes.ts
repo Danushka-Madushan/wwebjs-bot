cd8295ff1276f9cc56110310b4a9f5ea8d31b9c5
Danushka-Madushan <Danushkamadushan128@gmail.com>
2023-07-28 05:02:28 +0530

    base router

--- src/routes/base-routes.ts
+++ src/routes/base-routes.ts
@@ -1,19 +1,12 @@
 import { Router } from 'express';
-import { ExpressResponse } from '../core/utils/response.js';
-import { WApp } from '../core/app/web.js';
 
 const app = Router()
 
-app.get('/chats', async (req, res) => {
-    const rawData = await WApp.getChats()
-    const chats = rawData.map(({ id, name, isGroup }) => {
-        return {
-            id: id,
-            name: name,
-            isGroup: isGroup
-        }
-    })
-    return ExpressResponse(res, true, 200, chats)
-})
+import Webhooks from './webhooks.js'
+import Messages from './messages.js'
+
+app.use('/webhooks', Webhooks)
+
+app.use('/messages', Messages)
 
 export default app
