80990235c874c0224c9744ffe51534befcdc09b3
Danushka-Madushan <Danushkamadushan128@gmail.com>
2023-07-28 05:01:15 +0530

    message routing

---
+++ src/routes/messages.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import { ExpressResponse } from '../core/utils/response.js';
+import { WApp } from '../core/app/web.js';
+
+const app = Router()
+
+/* Get currently active chats */
+app.get('/chats', async (req, res) => {
+    const rawData = await WApp.getChats()
+    const chats = rawData.map(({ id, name, isGroup }) => {
+        return {
+            id: id,
+            name: name,
+            isGroup: isGroup
+        }
+    })
+    return ExpressResponse(res, true, 200, chats)
+})
+
+export default app
